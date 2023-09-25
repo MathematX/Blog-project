51c74d3e9972ec439d89f7759bbed4665e3b8cad
Máté Holló <hollomate1106@gmail.com>
2023-09-25 19:47:16 +0200

    If this commit is applied it will clear textbox and textarea after submit

--- scripts/script.js
+++ scripts/script.js
@@ -22,22 +22,22 @@ let form = document.getElementById("post-form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let title = document.getElementById("txt-title").value;
-  let content = document.getElementById("content").value;
+  let title = document.getElementById("txt-title");
+  let content = document.getElementById("content");
   
-  if (title !== "" && content !== "") {
+  if (title.value !== "" && content.value !== "") {
     let post = `<li style="list-style: none">
                   <div class="post">
-                    <h1>${title}</h1>
+                    <h1>${title.value}</h1>
                   
                     <p>
-                      ${content}
+                      ${content.value}
                     </p>
                   </div>
                 </li>`;
 
     document.getElementById("add-post-here").insertAdjacentHTML("afterbegin", post);
-    title = "";
-    content = "";
+    title.value = "";
+    content.value = "";
   }
 });
